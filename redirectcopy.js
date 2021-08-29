86cc661867c9b5c2ccb0d9d2274d5248dc81cb91
Thanh-Huyen-15 <69520587+Thanh-Huyen-15@users.noreply.github.com>
2021-08-29 10:42:44 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://review-duan-hoabinh.blogspot.com/'>https://review-duan-hoabinh.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.parasol-camranh.site/'>https://www.parasol-camranh.site/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
